4725c1465e503e9adb1950d021e2e867da3d2240
Logan Arnett <logan@loganarnett.com>
2019-05-04 01:02:09 -0400

    updating support helpers

--- spec/javascript/support/puppeteer/steps/sharedSteps.js
+++ spec/javascript/support/puppeteer/steps/sharedSteps.js
@@ -1,34 +1,32 @@
-const HOST = 'http://localhost:3000'
+import { TEST_USER_EMAIL, TEST_USER_PASSWORD } from '../consts'
+const HOST = 'http://localhost:3000/'
 
-const loginAsAgent = async (page) => {
-  await page.goto('http://localhost:3000/')
-  await page.waitForSelector('#root')
-
-  // Sign in
-  await page.click('.sign-in-btn')
-  await page.waitForNavigation()
-
-  // Salesforce login
-  await page.waitForSelector('#username_container')
-  await page.type('#username', process.env.E2E_SALESFORCE_USERNAME)
-  await page.type('#password', process.env.E2E_SALESFORCE_PASSWORD)
-  await page.click('#Login')
-
-  await page.waitForNavigation()
-  await page.waitForSelector('#root')
-}
-
-const goto = async (page, path) => {
+export const goto = async (page, path) => {
   await page.goto(`${HOST}${path}`)
   await waitForApp(page)
 }
 
-const waitForApp = async (page) => {
+export const loginAsAgent = async (page) => {
+  await goto(page, 'users/sign_in')
+
+  await page.type('#user_email', TEST_USER_EMAIL)
+  await page.type('#user_password', TEST_USER_PASSWORD)
+
+  await page.click('.actions input[type=submit]')
+
+  await page.waitForNavigation()
   await page.waitForSelector('#root')
 }
 
-export default {
-  loginAsAgent,
-  goto,
-  waitForApp
+export const waitForApp = async (page) => {
+  await page.waitForSelector('#root')
+}
+
+export const enterValue = async (page, selector, value) => {
+  // Wait for the field to appear
+  await page.waitForSelector(selector)
+  // Clear the value that's there
+  await page.$eval(selector, (el) => { el.value = '' })
+  // Enter the value
+  await page.type(selector, value)
 }
