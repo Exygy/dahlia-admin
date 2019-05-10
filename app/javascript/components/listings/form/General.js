78c280736a5ae86d47bb10dd627725131fbc1cfb
Logan Arnett <logan@loganarnett.com>
2019-05-10 18:57:29 -0400

    refactored to accommodate multiple types

--- app/javascript/components/listings/form/General.js
+++ app/javascript/components/listings/form/General.js
@@ -3,36 +3,35 @@ import { Field } from 'react-final-form'
 import FormGroup from '../../atoms/FormGroup'
 import { Required, MustBeNumberOrDashes } from '~/utils/final-form/validations'
 import { SimpleField } from '~/utils/formik/SimpleField'
-import { SimpleCheckbox } from '~/utils/final-form/SimpleCheckbox'
 import { SimpleDatePicker } from '~/utils/final-form/SimpleDatePicker'
 
-const ListingFormSectionsGeneral = ({ listing }) => {
+const ListingFormSectionsGeneral = ({ values }) => {
   return (
     <React.Fragment>
       <h3>General</h3>
 
       <FormGroup>
-        <SimpleField name='name' label='Name' validation={Required} />
-        <SimpleField name='image_url' label='Image URL' />
-        <SimpleField name='marketing_url' label='Marketing URL' />
-        <SimpleField name='priorities_descriptor' label='Priorities descriptor' />
-        <SimpleField name='program_type' label='Program type' />
+        <SimpleField name='name' label='Name' validation={Required} value={values.name || ''} />
+        <SimpleField name='image_url' label='Image URL' value={values.image_url || ''} />
+        <SimpleField name='marketing_url' label='Marketing URL' value={values.marketing_url || ''} />
+        <SimpleField name='priorities_descriptor' label='Priorities descriptor' value={values.priorities_descriptor || ''} />
+        <SimpleField name='program_type' label='Program type' value={values.program_type || ''} />
       </FormGroup>
 
       <br />
       <h3>Application</h3>
 
       <FormGroup>
-        <SimpleField name='application_organization' label='Organization' validation={Required} />
-        <SimpleField name='application_city' label='City' validation={Required} />
-        <SimpleField name='application_phone' label='Phone' validation={Required} />
-        <SimpleField name='application_postal_code' label='Zip Code' validation={MustBeNumberOrDashes} />
+        <SimpleField name='application_organization' label='Organization' validation={Required} value={values.application_organization || ''} />
+        <SimpleField name='application_city' label='City' validation={Required} value={values.application_city || ''} />
+        <SimpleField name='application_phone' label='Phone' validation={Required} value={values.application_phone || ''} />
+        <SimpleField name='application_postal_code' label='Zip Code' validation={MustBeNumberOrDashes} value={values.application_postal_code || ''} />
         <Field component={SimpleDatePicker} name='application_due_date' label='Due Date' />
         <br />
-        <SimpleField name='general_application_total' label='General application total' validation={MustBeNumberOrDashes} />
-        <SimpleCheckbox name='accepting_applications_by_po_box' label='Accepting applications by PO box?' />
-        <SimpleCheckbox name='accepting_applications_at_leasing_agent' label='Accepting applications at leasing agent?' />
-        <SimpleCheckbox name='blank_paper_application_can_be_picked_up' label='Blank paper application can be picked up?' />
+        <SimpleField name='general_application_total' label='General application total' validation={MustBeNumberOrDashes} value={values.general_application_total || ''} />
+        <SimpleField name='accepting_applications_by_po_box' label='Accepting applications by PO box?' type='checkbox' />
+        <SimpleField name='accepting_applications_at_leasing_agent' label='Accepting applications at leasing agent?' type='checkbox' />
+        <SimpleField name='blank_paper_application_can_be_picked_up' label='Blank paper application can be picked up?' type='checkbox' />
       </FormGroup>
     </React.Fragment>
   )
--- app/javascript/utils/formik/SimpleField.js
+++ app/javascript/utils/formik/SimpleField.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Field } from 'formik'
 
-export const SimpleField = ({ name, label, validation, ...fieldProps }) => (
+export const SimpleField = ({ name, label, validation, type, ...fieldProps }) => (
   <Field name={name} validate={validation} {...fieldProps} >
     {({ field, form: { touched, errors } }) => {
       const isTouched = touched[name]
       const error = errors[name]
       return (
         <React.Fragment>
-          <label htmlFor={`form-${name}`}>{label}:</label>
+          {type !== 'checkbox' && <label htmlFor={`form-${name}`}>{label}:</label>}
           <input
             id={`form-${name}`}
             className={(error && isTouched && 'error') || ''}
-            {...field} />
+            type={type || 'text'}
+            {...Object.assign(field, fieldProps)} />
+          {type && type === 'checkbox' && <label htmlFor={`form-${name}`}>{label}:</label>}
           {error && isTouched && <span className='error'>{error}</span>}
         </React.Fragment>
       )
