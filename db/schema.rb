a2c1ffe68001e692a270a59750b3ef8e9155ce57
Catherine Callaghan <catherine.callaghan@exygy.com>
2019-05-21 10:51:20 -0700

    [#164585235] Add migration for group's Google Tag Manager key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190507052502) do
+ActiveRecord::Schema.define(version: 2019_05_21_173626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20190507052502) do
     t.integer "children_count", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "gtm_key"
     t.index ["lft"], name: "index_groups_on_lft"
     t.index ["parent_id"], name: "index_groups_on_parent_id"
     t.index ["rgt"], name: "index_groups_on_rgt"
