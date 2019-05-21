103a3513b58918188cb3d53a0aaf3ec31be4d782
Catherine Callaghan <catherine.callaghan@exygy.com>
2019-05-21 16:56:44 -0700

    [#166172190] Replace the seeds file with the one generated by the gem
    We've added a seeds file template to the dahlia_data_models gem, so now running rails g dahlia_data_models:install will copy over that seeds file into the dahlia_admin app. We'll be maintaining the seeds file in the gem from now on, and only updating the seeds file in this repo with the generated version from the gem.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,52 @@
 # frozen_string_literal: true
 
-if Group.count.zero?
-  Group.create(
-    name: 'San Mateo County',
-    slug: 'smc',
-    domain: 'herokuapp.com',
-    group_type: :county,
-  )
-  Group.create(
-    name: 'San Jose',
-    slug: 'sj',
-    domain: 'localhost',
-  )
+def parse_from_json_file(filename)
+  json_file_str = File.read "lib/generators/dahlia_data_models/install/seed_data/#{filename}"
+  JSON.parse(json_file_str)
 end
 
-sample_listing_file = File.read 'lib/sample_data/sample_listings.json'
-sample_listings = JSON.parse(sample_listing_file)
-sample_listings.each do |l|
-  Listing.create(l) unless Listing.exists?(listing_id: l['listing_id'])
+def create_listing_from_file(filename, group)
+  listing_fields = parse_from_json_file(filename)
+
+  return false if Listing.exists?(listing_id: listing_fields['listing_id'])
+
+  listing = Listing.create(listing_fields)
+  listing.group = group
+  listing.save
+  listing
+end
+
+def create_groups
+  groups_fields = parse_from_json_file('groups.json')
+  groups_fields.each do |fields|
+    next if Group.exists?(slug: fields['slug'])
+    Group.create(fields)
+  end
+end
+
+def create_unit(listing)
+  unit_fields = parse_from_json_file('unit.json')
+  unit = Unit.create(unit_fields)
+  unit.listing = listing
+  unit.save
+end
+
+# Create test data as well as the placeholder groups and listings that we're
+# using to represent the real SMC and SJ groups and listings. Once Phase 1
+# of regional DAHLIA is launched, we will have finalized the actual data
+# for the real SMC and SJ groups and listings and will remove the SMC and SJ
+# placeholder data. We will keep the test data and add more test data.
+create_groups
+
+%w[testlocal testheroku smc sj].each do |slug|
+  listing = create_listing_from_file(
+    "#{slug}_listing.json",
+    Group.find_by(slug: slug),
+  )
+
+  next unless listing
+
+  create_unit(listing)
 end
 
 if Rails.env.test?
